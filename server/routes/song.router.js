595c8ab73dc2856c0111434ad291e8e3b272802d
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-07 14:20:37 -0500

    returns data from database in songs.router

--- server/routes/song.router.js
+++ server/routes/song.router.js
@@ -1,28 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const pg = require('pg');
+
+const Pool = pg.Pool; // Class
+
+// Connect Node to our database
+const pool = new Pool({
+  database: 'jazzy_ajax', // name of our database
+  host: 'localhost', // where is your database?
+  port: 5432, // this is the default port
+  max: 10, // number of connections
+  idleTimeoutMillis: 10000, // 10 seconds
+});
+
+router.get('/', (req, res) => {
+  console.log(`In /songs GET`);
+
+  let queryText = `SELECT * FROM "songs";`;
+  pool
+    .query(queryText)
+    .then((result) => {
+      // send back our query results as an array of objects
+      res.send(result.rows); // result.rows will always be an Array
+    })
+    .catch((error) => {
+      console.log(`Error in GET /songs ${error}`);
+      // 500 means "server error", generic but effective
+      res.sendStatus(500);
+    });
+});
 
 // static content. this will be replaced with a database table
 const songListArray = [
-    {
-        title: 'Take Five',
-        length: '2:55',
-        date_released: '1959-09-29'
-    },
-    {
-        title: 'So What',
-        length: '9:22',
-        date_released: '1959-08-17'
-    }
+  {
+    title: 'Take Five',
+    length: '2:55',
+    date_released: '1959-09-29',
+  },
+  {
+    title: 'So What',
+    length: '9:22',
+    date_released: '1959-08-17',
+  },
 ];
-
-router.get('/', (req, res) => {
-    console.log(`In /songs GET`);
-    res.send(songListArray);
-});
-
-router.post('/', (req, res) => {
-    
-    res.sendStatus(201);
-});
 
 module.exports = router;
